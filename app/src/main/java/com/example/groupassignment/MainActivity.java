fb9635c6993341a3d5a33bbf6759cd426f2604f0
TeemuTo <tmatossavainen@gmail.com>
2022-04-28 14:37:35 +0300

    New changelist

--- app/src/main/java/com/example/groupassignment/MainActivity.java
+++ app/src/main/java/com/example/groupassignment/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.groupassignment;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.groupassignment.databinding.ActivityMainBinding;
 import com.google.android.material.button.MaterialButton;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView authstatus;
+    private Button biobutton;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     EditText username;
     EditText password;
@@ -26,6 +38,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        authstatus = findViewById(R.id.authstatus);
+        biobutton = findViewById(R.id.biobutton);
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                //error authentication, stop task
+                super.onAuthenticationError(errorCode, errString);
+                authstatus.setText("Authentication error: "+ errString);
+                Toast.makeText(MainActivity.this, "Authentication error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //authentication succeed
+                authstatus.setText("Authentication succeed");
+                Toast.makeText(MainActivity.this, "Authentication succeed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //failed auth...
+                authstatus.setText("Authentication failed");
+                Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setSubtitle("Login using fingerprint")
+                .setNegativeButtonText("Use App password")
+                .build();
+
+        biobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
 
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
